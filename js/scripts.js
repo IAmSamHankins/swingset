8c8331c260c6ead9975f8f163e0b40eb77d308d0
Samantha Hankins <sam@iamsamhankins.com>
2016-10-04 18:45:27 -0400

    basic replies and threads

--- js/scripts.js
+++ js/scripts.js
@@ -5,17 +5,29 @@
 //});
 
 $(document).ready(function(){
-    $("form").submit(function(){
+    $(".commentBox").submit(function(){
         if ($("#commentInput").val()){
             var comment = $("#commentInput").val();
             var commenterName = $("#commenterNameInput").val();
-            $('.theComments').prepend('<div class="commentStyle">' + '<p class="commenterName">' + commenterName + '</p>' + '<p>' + comment + '</p>' + '<div class="actionBar"> <button class="replyButton"><i class="fa fa-mail-reply fa-lg"></i>&nbsp Reply</button> <button><i class="fa fa-link fa-lg"></i>&nbsp Link</button> <button><i class="fa fa-flag fa-lg"></i>&nbsp Flag</button></div>' + '<form class="replyBox"><div class="row"><label for="commenterNameInput">Your Name &nbsp<a href="#">Log In</a></label> <input class="u-full-width" type="text" placeholder="e.g. FoodieFooderson" id="commenterNameInput"></div> <div class="row"><label for="commentInput">Your Comment</label><textarea class="u-full-width" placeholder="Your comment here..." id="commentInput"></textarea></div><div class="row"><div class="six columns"><label class="agree-tos"><input type="checkbox"><span class="label-body">Read Terms of Service</span></label></div><div class="six columns"><input class="button-primary u-pull-right" type="submit" value="submit reply"></div></div></form>' +'</div>');
+            $('.theComments').prepend('<div class="commentStyle">' + '<p class="commenterName">' + commenterName + '</p>' + '<p>' + comment + '</p>' + '<div class="actionBar"> <button class="replyButton"><i class="fa fa-mail-reply fa-lg"></i>&nbsp Reply</button> <button><i class="fa fa-link fa-lg"></i>&nbsp Link</button> <button><i class="fa fa-flag fa-lg"></i>&nbsp Flag</button></div>' + '<form class="replyBox"><div class="row"><label for="replyNameInput">Your Name &nbsp<a href="#">Log In</a></label> <input class="u-full-width" type="text" placeholder="e.g. Ham4Hamilton" id="replyNameInput"></div> <div class="row"><label for="replyInput">Your Comment</label><textarea class="u-full-width" placeholder="Your comment here..." id="replyInput"></textarea></div><div class="row"><div class="six columns"><label class="agree-tos"><input type="checkbox"><span class="label-body">Read Terms of Service</span></label></div><div class="six columns"><input class="button-primary u-pull-right" type="submit" value="submit reply"></div></div></form><div class="repliesArea"></div>'+'</div>');
         }
         $("#commentInput").val('');
         $("#commenterNameInput").val('');
         return false;
     });
-    $(document.body).on("click", ".replyButton", function(){
+    $(document.body).on("submit", ".replyBox", function() {
+        if ($(this).parent().find("#replyInput").val()){
+        var reply = $(this).parent().find("#replyInput").val();
+        var replyName = $(this).parent().find("#replyNameInput").val();
+        $(this).closest('.commentStyle').find('.repliesArea').append('<div class="replyStyle">' + '<p class="commenterName">' + replyName + '</p>' + '<p>' + reply + '</p>' + '<div class="actionBar"><button><i class="fa fa-link fa-lg"></i>&nbsp Link</button> <button><i class="fa fa-flag fa-lg"></i>&nbsp Flag</button></div>' +'</div>');
+        };
+        $(this).closest('.commentStyle').find('.replyBox').slideUp();
+        $("#replyInput").val('');
+        $("#replyNameInput").val('');
+        return false;
+    });
+        $(document.body).on("click", ".replyButton", function(){
         $(this).closest('.commentStyle').find('.replyBox').slideToggle();
     });
+    
 });
